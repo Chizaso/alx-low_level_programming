10b126597aaafb982ee64867322a2758e9ad3473
Chizaso <johnnkhata@gmail.com>
2022-09-13 10:23:02 -0700

    writting function that computers the absolute values of intergers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main,h"
+/**
+ * _abs -> return absolute value of numbers
+ * @n: parameters
+ * Return: absolute value of number.
+ */
+int _abs(iny n)
+{
+	if (n > 0)
+	return (n);
+	else
+	return (-n);
+}
