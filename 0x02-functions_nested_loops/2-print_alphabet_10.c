b2160ef2205a8dd0f2ea54f6e8f2a6271f0ee339
Chizaso <johnnkhata@gmail.com>
2022-09-13 04:19:31 -0700

    Done with the task alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> print the lower case alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int p;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (p = 'a'; p <= 'z'; p++)
+		{
+			_putchar(p);
+		}
+
+	_putchar('\n');
+
+	}
+
+}
+	
