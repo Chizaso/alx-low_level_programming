eda0dbbf23fadbf60c7e228c9fc9d33bd8d2c673
Chizaso <johnnkhata@gmail.com>
2022-09-13 23:28:05 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,22 @@
-#include  "main.h"
+#include "main.h"
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * print_alphabet_x10 -> print the lower case alphabet
  */
-void times_table(void)
-{
-	int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
+void print_alphabet_x10(void)
+{
+	int p;
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
+
+		for (p = 'a'; p <= 'z'; p++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			_putchar(p);
 		}
-		_putchar('\n');
+
+	_putchar('\n');
+
 	}
 }
