9fc3285c41eea3539fd709df800538a003db9b17
Chizaso <johnnkhata@gmail.com>
2022-12-29 02:04:23 -0800

    done

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower -> Checks if a charater is lowercase.
+ * @c: The charater to be checked.
+ * Return: 1 if charater is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -> return absolute value of numbers
+ * @n: parameters
+ * Return: absolute value of number.
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	return (n);
+	else
+	return (-n);
+}
